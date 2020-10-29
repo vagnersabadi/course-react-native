a01fe827c9f2497b6e66e48e21fe99845fe61692
Vagner Sabadi <vagnerbuzatta@hotmail.com>
2020-10-28 21:48:46 -0300

    feat: create form custom if FormRow

--- series/App.js
+++ series/App.js
@@ -9,7 +9,21 @@ const Stack = createStackNavigator();
 function AppContainer() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login"
+        screenOptions={{
+          title: "Series!",
+          headerTintColor: 'white',
+          headerStyle: {
+              backgroundColor: '#6ca2f7',
+              borderBottomWidth: 1,
+              borderBottomColor: '#C5C5C5',
+          },
+          headerTitleStyle: {
+              color: 'white',
+              fontSize: 30,
+          }
+        }}
+      >
         <Stack.Screen name="Login" component={LoginPage} options={{ title: 'Bem vindo!' }} />
       </Stack.Navigator>
     </NavigationContainer>
--- series/src/pages/login/LoginPage.js
+++ series/src/pages/login/LoginPage.js
@@ -8,16 +8,42 @@ import {
     ActivityIndicator,
     Alert
 } from 'react-native';
+import FormRow from '../../components/FormRow';
 
 export class LoginPage extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>Login Test</Text>
-                
+            <View style={styles.container}>
+                <FormRow>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="user@mail.com" />
+                </FormRow>
+
+                <FormRow>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="*******"
+                        secureTextEntry
+                    />
+                </FormRow>
             </View>
         )
     }
 
 };
+
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    input: {
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingBottom: 5,
+    },
+});
